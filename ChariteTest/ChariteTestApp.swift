94e2f33529258cae3c222ee5376d9794427fb3ae
Alonso Essenwanger <alonsohe@hotmail.com>
2020-08-13 14:14:30 +0200

    Initial Commit

---
+++ ChariteTest/ChariteTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChariteTestApp.swift
+//  ChariteTest
+//
+//  Created by Alonso Essenwanger on 13.08.20.
+//
+
+import SwiftUI
+
+@main
+struct ChariteTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChariteTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ChariteTest
+//
+//  Created by Alonso Essenwanger on 13.08.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
